f15563baa377336576d85ca1f11c68c337a47cec
JoyceZ-J <603867773@qq.com>
2021-05-20 17:03:39 +0800

    修改了表名与实体名映射

--- src/main/java/com/zj/jpademo/domain/Scholar.java
+++ src/main/java/com/zj/jpademo/domain/Scholar.java
@@ -6,13 +6,12 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity(name = "Scholar")
-@Table
+@Table(name = "Scholars")//设置数据库中表名字
 public class Scholar {
     @Id
     private Integer id;
-    private String personID;
-    private String orgID;
     private String name;
+    private String orgID;
     private String sex;
     private String department;
     private String postRank;
@@ -20,18 +19,20 @@ public class Scholar {
     private String tel;
     private String email;
     private String eduBackg;
+    private String tutor;
     private String students;
+    private String patents;
+    private String papers;
+
 
     /*构造函数*/
-
     public Scholar() {
     }
 
-    public Scholar(Integer id, String personID, String orgID, String name, String sex, String department, String postRank, String fieldofStudy, String tel, String email, String eduBackg, String students) {
+    public Scholar(Integer id, String name, String orgID, String sex, String department, String postRank, String fieldofStudy, String tel, String email, String eduBackg, String tutor, String students, String patents, String papers) {
         this.id = id;
-        this.personID = personID;
-        this.orgID = orgID;
         this.name = name;
+        this.orgID = orgID;
         this.sex = sex;
         this.department = department;
         this.postRank = postRank;
@@ -39,25 +40,12 @@ public class Scholar {
         this.tel = tel;
         this.email = email;
         this.eduBackg = eduBackg;
+        this.tutor = tutor;
         this.students = students;
+        this.patents = patents;
+        this.papers = papers;
     }
-
-    public String toString() {
-        return "Scholar{" +
-                "id=" + id +
-                ", personID='" + personID + '\'' +
-                ", orgID='" + orgID + '\'' +
-                ", name='" + name + '\'' +
-                ", sex='" + sex + '\'' +
-                ", department='" + department + '\'' +
-                ", postRnak='" + postRank + '\'' +
-                ", fieldofStudy='" + fieldofStudy + '\'' +
-                ", tel='" + tel + '\'' +
-                ", email='" + email + '\'' +
-                ", eduBackg='" + eduBackg + '\'' +
-                ", students=" + students +
-                '}';
-    }
+    /*set,get*/
 
     public Integer getId() {
         return id;
@@ -67,12 +55,12 @@ public class Scholar {
         this.id = id;
     }
 
-    public String getPersonID() {
-        return personID;
+    public String getName() {
+        return name;
     }
 
-    public void setPersonID(String personID) {
-        this.personID = personID;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getOrgID() {
@@ -81,14 +69,6 @@ public class Scholar {
 
     public void setOrgID(String orgID) {
         this.orgID = orgID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getSex() {
@@ -147,11 +127,55 @@ public class Scholar {
         this.eduBackg = eduBackg;
     }
 
+    public String getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(String tutor) {
+        this.tutor = tutor;
+    }
+
     public String getStudents() {
         return students;
     }
 
     public void setStudents(String students) {
         this.students = students;
+    }
+
+    public String getPatents() {
+        return patents;
+    }
+
+    public void setPatents(String patents) {
+        this.patents = patents;
+    }
+
+    public String getPapers() {
+        return papers;
+    }
+
+    public void setPapers(String papers) {
+        this.papers = papers;
+    }
+
+    @Override
+    public String toString() {
+        return "Scholar{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", orgID='" + orgID + '\'' +
+                ", sex='" + sex + '\'' +
+                ", department='" + department + '\'' +
+                ", postRank='" + postRank + '\'' +
+                ", fieldofStudy='" + fieldofStudy + '\'' +
+                ", tel='" + tel + '\'' +
+                ", email='" + email + '\'' +
+                ", eduBackg='" + eduBackg + '\'' +
+                ", tutor='" + tutor + '\'' +
+                ", students='" + students + '\'' +
+                ", patents='" + patents + '\'' +
+                ", papers='" + papers + '\'' +
+                '}';
     }
 }
